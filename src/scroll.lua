e6f4f76e490013e442fb9f0af7c1f68a246c776d
Ayu <claire@ayu.land>
2024-04-15 01:38:42 +0800

    🚧 Intertia in scroll

--- src/scroll.lua
+++ src/scroll.lua
@@ -7,12 +7,19 @@ return function ()
   local hx  -- Held start position
   local hxo -- Held offset (`dx` = pointer x + `hxo`)
   local captured = false
+  -- Ring buffer of history `dx`
+  local history, history_ptr
+  local HISTORY_WINDOW = 40
+
+  local intertia_v = 0
+  local DECEL = 0.2
 
   s.press = function (x, y)
     held = true
     hx = x
     hxo = s.dx - x
     captured = false
+    history, history_ptr = {}, HISTORY_WINDOW
   end
 
   -- Return values
@@ -25,6 +32,7 @@ return function ()
     if (x - hx) * (x - hx) >= 400 then
       if not captured then
         -- Start capture
+        captured = true
         return 2
       end
     elseif not captured then
@@ -34,11 +42,38 @@ return function ()
   end
 
   s.release = function (x, y)
+    if held and captured then
+      -- Calculate velocity
+      local time, delta
+      if #history < HISTORY_WINDOW then
+        time = #history - 1
+        delta = history[#history] - history[1]
+      else
+        time = HISTORY_WINDOW - 1
+        delta = history[history_ptr] - history[history_ptr % HISTORY_WINDOW + 1]
+      end
+      intertia_v = delta / time
+    end
+
     held = false
     captured = false
+    history, history_ptr = nil, nil
   end
 
   s.update = function ()
+    if held and captured then
+      if history_ptr == HISTORY_WINDOW then
+        history_ptr = 1
+      else
+        history_ptr = history_ptr + 1
+      end
+      history[history_ptr] = s.dx
+    elseif intertia_v ~= 0 then
+      local sign = (intertia_v < 0 and -1 or 1)
+      local next_v = (math.abs(intertia_v) < DECEL and 0 or intertia_v - sign * DECEL)
+      s.dx = s.dx + (intertia_v + next_v) / 2
+      intertia_v = next_v
+    end
   end
 
   return s
