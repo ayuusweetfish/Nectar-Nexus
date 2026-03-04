5714453b14b95cda30a90a255e20f0261b8b5cef
Ayu <claire@ayu.land>
2026-03-04 17:53:04 +0800

    📦 Update packaging for web
    Blow-Something@1c3e419

--- misc/build_web/build.sh
+++ misc/build_web/build.sh
@@ -1,7 +1,27 @@
-rm Nectar_Nexus.love
-zip Nectar_Nexus.love -r main.lua src img aud -9
+mkdir -p release
+rm -f release/Nectar_Nexus.love
+t=$(mktemp -d)
+wd=$PWD
+cp -pr main.lua aud img src "$t/"
+(
+  cd "$t/" || exit
+  pwd
+  if [ "$1" = "min" ]; then
+    for i in $(find . -type f -name '*.lua'); do
+      node "$wd/misc/build_web/luamin-env/node_modules/luamin/bin/luamin" -f "$i" > "$t/_tmp"
+      mv "$t/_tmp" "$i"
+    done
+  fi
+  find . -exec touch -t 198001010000 {} +
+  find . -type f -print | sort | zip "$wd/release/Nectar_Nexus.love" -X -@ -9
+  echo "$wd/release/Nectar_Nexus.love"
+  sha1sum "$wd/release/Nectar_Nexus.love"
+)
+rm -rf "$t"
 
-rm -rf Nectar-Nexus-web
-~/Downloads/node-v20.12.2-darwin-x64/bin/node ~/Downloads/lovejs-11/love.js/index.js --title "Nectar Nexus" Nectar_Nexus.love -m 134217728 -c Nectar-Nexus-web
-cp misc/build/index.html Nectar-Nexus-web
-rm -rf Nectar-Nexus-web/theme
+LOVEJS_INDEX=misc/build_web/lovejs-env/node_modules/love.js/index.js
+
+rm -rf release/Nectar-Nexus-web
+node ${LOVEJS_INDEX} --title "Nectar Nexus" release/Nectar_Nexus.love -m 134217728 -c release/Nectar-Nexus-web
+cp misc/build_web/index.html release/Nectar-Nexus-web
+rm -rf release/Nectar-Nexus-web/theme
